a3b306c7b8b47ed4e13a83df7fdd887d18b48ed9
Adam Craven <adammcraven@gmail.com>
2016-01-31 21:16:20 +1000

    New approach to create default config file in volume

---
+++ consul-agent/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# See https://www.consul.io/docs/agent/options.html for config file options
+
+configFile="/var/lib/consul/config/100-default-agent-config.generated.json"
+rm -f $configFile
+
+echo "==> Creating the default Consul agent config file: $configFile"
+( cat <<"EOM"
+{
+	"client_addr": "0.0.0.0",
+	"data_dir": "/var/lib/consul/data",
+    "disable_remote_exec": true,
+	"dns_config": {
+		"allow_stale": true,
+		"max_stale": "1s"
+	},
+	"leave_on_terminate": true
+}
+EOM
+) >> $configFile
+
+echo "==> Running Consul agent with additional args: '$@'"
+exec "/bin/consul" "agent" "-config-dir=/var/lib/consul/config" "$@"
---
+++ consul-server/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# See https://www.consul.io/docs/agent/options.html for config file options
+
+configFile="/var/lib/consul/config/100-default-server-config.generated.json"
+rm -f $configFile
+
+echo "==> Creating the default Consul server config file: $configFile"
+( cat <<"EOM"
+{
+	"client_addr": "0.0.0.0",
+	"data_dir": "/var/lib/consul/data",
+    "disable_remote_exec": true,
+	"dns_config": {
+		"allow_stale": false
+	},
+	"leave_on_terminate": true,
+	"ui": true
+}
+EOM
+) >> $configFile
+
+echo "==> Running Consul server with additional args: '$@'"
+exec "/bin/consul" "agent" "-server" "-config-dir=/var/lib/consul/config" "$@"
